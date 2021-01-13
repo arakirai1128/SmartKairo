1dc1b34b76370e2d887cc778fa0eb08b40ea06f9
荒木礼 <araki@kdl.co.jp>
2021-01-13 19:16:54 +0900

    two commit

--- src/components/RadialGauge.tsx
+++ src/components/RadialGauge.tsx
@@ -9,7 +9,7 @@ const useStyles = makeStyles((theme: any) => ({
     root: {
         position: 'relative',
         bottom: 120,
-        left: 210,
+        left: 206,
         fontSize: 27,
 
     }
